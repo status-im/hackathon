8738b5ad3aec3d7aeb17457ac8f96d2dc1d81611
gjeanmart <gregoire.jeanmart@gmail.com>
2017-06-07 20:41:24 +0000

    conf

--- submissions/dts/truffle.js
+++ submissions/dts/truffle.js
@@ -6,7 +6,7 @@ module.exports = {
       network_id: "*" 
     },  
     development: {
-      host: "192.168.0.23", // Match any network id
+      host: "0.0.0.0", // Match any network id
       port: 8545,
       network_id: "*" // Match any network id
     }, 
