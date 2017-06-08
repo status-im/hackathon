fbc1dff8a58ffbc46442e30c7dfb3a95048746cc
gjeanmart <gregoire.jeanmart@ai-london.com>
2017-06-08 19:50:26 +0100

    ropsten on 8545

--- submissions/dts/truffle.js
+++ submissions/dts/truffle.js
@@ -2,17 +2,17 @@ module.exports = {
   networks: {
     localhost: {
       host: "localhost", 
-      port: 8545,
+      port: 8546,
       network_id: "*" 
     },  
     development: {
       host: "0.0.0.0", // Match any network id
-      port: 8545,
+      port: 8546,
       network_id: "*" // Match any network id
     }, 
     ropsten: {
       host: "0.0.0.0",
-      port: 8546,
+      port: 8545,
       network_id: "3" // Match any network id
     }
   }
